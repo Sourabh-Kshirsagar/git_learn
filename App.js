81645e6ade90f3678eab6db31af6efcd11b0eb8e
SourabhKshirsagar-sk <kshirsagar.sourabh.sk@gmail.com>
2023-08-29 18:48:51 +0530

    class 04 commit

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.css";
 
 /***
  *
@@ -24,67 +25,154 @@ import ReactDOM from "react-dom/client";
  *
  * Transitive Dependencies
  */
-const heading = React.createElement(
-  "h1",
-  {
-    id: "title",
-    key: "h1",
-  },
-  "This is heading one"
-);
+// const heading = React.createElement(
+//   "h1",
+//   {
+//     id: "title",
+//     key: "h1",
+//   },
+//   "This is heading one"
+// );
 
-const heading1 = React.createElement(
-  "h2",
-  {
-    id: "title",
-    key: "h2",
-  },
-  "This is heading Two"
-);
+// const heading1 = React.createElement(
+//   "h2",
+//   {
+//     id: "title",
+//     key: "h2",
+//   },
+//   "This is heading Two"
+// );
 
-const container = React.createElement(
-  "div",
-  {
-    id: "container",
-    key: "div",
-  },
-  [heading, heading1]
-);
+// const container = React.createElement(
+//   "div",
+//   {
+//     id: "container",
+//     key: "div",
+//   },
+//   [heading, heading1]
+// );
 
 //functional components
 
 // multiple ways to write functional components
 
-const HeadingReact = () => {
-  return <h1>This is the Example of functional component</h1>;
-};
+// const HeadingReact2 = () => (
+//   <div>
+//     <HeadingReact />
+//     {heading}
+//     <HeadingReact1 />
+//     <h1>This is second way to write functional component ...</h1>
+//     <h1>This is second way to write functional component ...</h1>
+//     <h1>This is second way to write functional component ... </h1>
+//   </div>
+// );
 
-const HeadingReact1 = () => {
+const title = React.createElement("h1", {}, "This is our title");
+
+const HeaderComponent = () => {
   return (
-    <div>
-      <HeadingReact />
-      {heading}
-      <h1>This is second way to write functional component</h1>
-      <h1>This is second way to write functional component</h1>
-      <h1>This is second way to write functional component</h1>
-    </div>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="index.html">
+          <img
+            className="logo-width"
+            src="https://intelliatechcom33628.zapwp.com/q:u/r:0/wp:1/w:1/u:https://intelliatech.com/wp-content/uploads/2023/07/logo-scaled.jpg"
+          ></img>
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse right-side-nav" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Features
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Pricing
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" aria-disabled="true">
+                Disabled
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
-const HeadingReact2 = () => (
-  <div>
-    <HeadingReact />
-    {heading}
-    <HeadingReact1 />
-    <h1>This is second way to write functional component ...</h1>
-    <h1>This is second way to write functional component ...</h1>
-    <h1>This is second way to write functional component ... </h1>
-  </div>
-);
+const restro = {
+  image:
+    "https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg",
+  name: "Restaurant Name",
+  text: "some location some location some location some location ",
+  rating: "3.6",
+};
+
+const RestourantCards = () => {
+  return (
+    <>
+      <div className="container mt-5">
+        <div className="card">
+          <img src={restro.image} className="card-img-top" alt="..."></img>
+          <div className="card-body">
+            <h5 className="card-title">{restro.name}</h5>
+            <p className="card-text">{restro.text}</p>
+            <p className="card-text">{restro.rating} Stars</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const BodyComponent = () => {
+  return (
+    <>
+      <div className="display-setting container">
+        <RestourantCards />
+        <RestourantCards />
+        <RestourantCards />
+      </div>
+    </>
+  );
+};
+
+const FooterComponent = () => {
+  return <div classNam="container-fluid">Footer</div>;
+};
+
+const AppLayout = () => {
+  return (
+    <>
+      <HeaderComponent />
+      <BodyComponent />
+      <FooterComponent />
+    </>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingReact2 />);
+root.render(<AppLayout />);
 
 // • Benefits of JSX
 // • Behind the Scenes of JSX
@@ -100,7 +188,7 @@ root.render(<HeadingReact2 />);
 // {TitleComponent} VS {<TitleComponent/>} VS {<Title Component></TitleComponent>} in JSX
 
 // Coding Assignment:
-// •Create a Nested header Element using React.createElement(h1, h2, h3 inside a div with class "title")
+// •Create a Nested header Element using React.createElement(h1, h2, h3 inside a div with classNam "title")
 // • Create the same element using JSX
 // • Create a functional component of the same with JSX
 // O Pass attributes into the tag in JSX
