a0741e39457c2dc3b04d02a1d3399df2e2f4dae9
SourabhKshirsagar-sk <kshirsagar.sourabh.sk@gmail.com>
2023-09-29 12:11:33 +0530

    class_07_a commit

--- src/components/BodyComponent.js
+++ src/components/BodyComponent.js
@@ -86,3 +86,11 @@ const BodyComponent = () => {
 };
 
 export default BodyComponent;
+
+// Assignment
+
+// What are various ways to add images into our App? Explain with code examples
+// What would happen if we do console.log(useState())?
+// How will useEffect behave if we don't add a dependency array ?
+// What is SPA?
+// What is difference between Client Side Routing and Server Side Routing?
