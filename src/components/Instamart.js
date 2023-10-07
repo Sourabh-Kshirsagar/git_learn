65628f2849d9889d0d60b9374924a34030171576
Sourabh-Kshirsagar <iamsk265@gmail.com>
2023-10-07 20:34:08 +0530

    Commit class 09 F

--- src/components/Instamart.js
+++ src/components/Instamart.js
@@ -1,6 +1,7 @@
 const Instamart = () => {
   return (
     <>
+      {/* This component is just crated to check the functionlity for the Dynamic Import or chunking or etc */}
       <h3>Instamart</h3>
       <h3>Having 100's of Component</h3>
     </>
