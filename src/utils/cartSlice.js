e101b583eaf4427995a9f2afd002b107e1eefc56
SourabhKshirsagar-sk <kshirsagar.sourabh.sk@gmail.com>
2023-10-19 19:07:04 +0530

    class 12 commit

--- src/utils/cartSlice.js
+++ src/utils/cartSlice.js
@@ -1,11 +1,39 @@
 import { createSlice } from "@reduxjs/toolkit";
-
-const slice = createSlice({
+// In this way we created the slices which is coming from redux tool kit
+const cartSlice = createSlice({
+  // Here we give the slice name
   name: "cart",
+  // initial state for the slices
   initialState: {
     items: [],
   },
+
+  // Here is the Reducer function with is triggered by the action to add the items
   reducers: {
-    addItem: () => {},
+    addItem: (state, action) => {
+      // The state is the initial state and the action is the data that is coming form the action
+      // of click to add the data in cart
+
+      // in this way we push the data to the action and update the state accordingly
+      state.items.push(action.payload);
+    },
+
+    // To remove an items we have something for this
+    removeItem: (state, action) => {
+      state.items.pop();
+    },
+
+    // in this way we clear or cart also
+    clearCart: (state) => {
+      state.items = [];
+    },
   },
 });
+
+// this is action here not the actions
+export const { addItem, removeItem, clearCart } = cartSlice.actions;
+
+// this is the reducer not reducers this is given by the redux toolkit
+export default createSlice.reducer;
+
+// NOW THIS SLICE WILL BE GO INTO OUT STORE!!!
--- src/utils/store.js
+++ src/utils/store.js
@@ -1,6 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import cartSlice from "./cartSlice";
 
 // This configureStore will take the slices and configure them in our store
-const store = configureStore({});
+const store = configureStore({
+  reducer: {
+    cart: cartSlice,
+  },
+});
 
 export default store;
