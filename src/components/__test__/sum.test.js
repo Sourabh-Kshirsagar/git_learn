bd6646f835126964f1af95744372289b4f278d0f
SourabhKshirsagar-sk <kshirsagar.sourabh.sk@gmail.com>
2023-11-01 17:39:12 +0530

    class 13 commit

---
+++ src/components/__test__/sum.test.js
@@ -0,0 +1,17 @@
+import { ansi16 } from "color-convert";
+import { sum } from "../sum";
+test("addition program", () => {
+  expect(sum(2, 4)).toBe(6);
+});
+
+// this test will take two things first is the string and the iother one is call back fn whch calls an
+// expect which take fn and the valuue to be passed to that function and the expected value in the toBe
+
+// Here we write the test case which is to be passed to the sum.js to generate the output
+
+// Install React Testing Librabry
+// Install Jest
+// Configure Jest
+// Installed jest-environnent-jsdo
+// Create my first test
+// Conigure babel in baelrc or bable.config.js file
---
+++ src/components/sum.js
@@ -0,0 +1,2 @@
+export const sum = (a, b) => a + b;
+// This is the file for to run the test case only
